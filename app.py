00342a6453bbd89e619bc26a4cae7fcb4b0181db
XTanishkX <122778914+XTanishkX@users.noreply.github.com>
2024-07-23 00:05:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import pickle
 import streamlit as st
 import requests
 import pandas as pd
+import gzip
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=3287a7ac2ca9ddaa6e4ed8b373654025".format(movie_id)
-    data = requests.get(url).json()
+    data = requests.get(url)
+    data = data.json()
     poster_path = data['poster_path']
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
@@ -23,15 +25,55 @@ def recommend(movie):
 
     return recommended_movie_names, recommended_movie_posters
 
+def download_file_from_google_drive(file_id, dest_path):
+    URL = "https://drive.google.com/uc?export=download"
+
+    session = requests.Session()
+    response = session.get(URL, params={'id': file_id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': file_id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, dest_path)
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
+
+def save_response_content(response, dest_path):
+    CHUNK_SIZE = 32768
+
+    with open(dest_path, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
 st.header('Movie Recommender System')
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+
 movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+# Google Drive file ID
+file_id = '1Bc3YJfYPKCz-UPRbOXUZ3ze-7YLLVgYM'
+
+# Destination path
+dest_path = 'similarity.pkl.gz'
+
+# Download the file from Google Drive
+download_file_from_google_drive(file_id, dest_path)
+
+# Load the similarity data from the compressed gzip file
+with gzip.open(dest_path, 'rb') as f:
+    similarity = pickle.load(f)
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
     "Type or select a movie from the dropdown",
-    movie_list
+    movies['title'].values
 )
 
 if st.button('Show Recommendation'):
@@ -52,3 +94,4 @@ if st.button('Show Recommendation'):
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
+
